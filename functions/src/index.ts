63eebf023d7ce5cb8264eb626e9524da7d08754e
Clément Contet <clement.contet@orange.com>
2020-09-30 09:07:03 +0200

    Update centers on path updates too

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,62 +1,69 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 import * as functions from 'firebase-functions';
+import {EventContext} from 'firebase-functions/lib/cloud-functions';
+import {QueryDocumentSnapshot} from 'firebase-functions/lib/providers/firestore';
 
 // The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-export const setPathCenter = functions.firestore.document('/items/{documentId}')
-  .onCreate((doc, context) => {
-    if (doc.data() == null) {
-      functions.logger.log('null data');
-      return null;
-    }
-    if (doc.data().geometry.type !== 'LineString') {
-      functions.logger.log('geometry.type !== \'LineString\'');
-      return null;
-    }
+export const setPathCenterCreation = functions.firestore.document('/items/{documentId}')
+  .onCreate(computeCenters);
 
-    // Nested array are not supported in Cloud Firestore (yet?)
-    // so 'coordinates' is stored as a dict and we change it back
-    // to an array
-    const lineCoords: number[][] = Object.values(doc.data().geometry.coordinates);
-    let lastX = lineCoords[0][0];
-    let lastY = lineCoords[0][1];
-    let boundingBoxMinX = lastX;
-    let boundingBoxMaxX = lastX;
-    let boundingBoxMinY = lastY;
-    let boundingBoxMaxY = lastY;
-    let distance = 0;
-    lineCoords.splice(0, 1);
+export const setPathCenterUpdate = functions.firestore.document('/items/{documentId}')
+  .onUpdate((change, context) => computeCenters(change.after, context));
 
-    for (const coord of lineCoords) {
-      const coordX = coord[0];
-      const coordY = coord[1];
-      distance = distance + Math.round(computeDistance(lastX, lastY, coordX, coordY) / 100) / 10;
-      if (coordX < boundingBoxMinX) {
-        boundingBoxMinX = coordX;
-      }
-      if (coordX > boundingBoxMaxX) {
-        boundingBoxMaxX = coordX;
-      }
-      if (coordY < boundingBoxMinY) {
-        boundingBoxMinY = coordY;
-      }
-      if (coordY > boundingBoxMaxY) {
-        boundingBoxMaxY = coordY;
-      }
-      lastX = coordX;
-      lastY = coordY;
+function computeCenters(doc: QueryDocumentSnapshot, context: EventContext) {
+  if (doc.data() == null) {
+    functions.logger.log('null data');
+    return null;
+  }
+  if (doc.data().geometry.type !== 'LineString') {
+    functions.logger.log('geometry.type !== \'LineString\'');
+    return null;
+  }
+
+  // Nested array are not supported in Cloud Firestore (yet?)
+  // so 'coordinates' is stored as a dict and we change it back
+  // to an array
+  const lineCoords: number[][] = Object.values(doc.data().geometry.coordinates);
+  let lastX = lineCoords[0][0];
+  let lastY = lineCoords[0][1];
+  let boundingBoxMinX = lastX;
+  let boundingBoxMaxX = lastX;
+  let boundingBoxMinY = lastY;
+  let boundingBoxMaxY = lastY;
+  let distance = 0;
+  lineCoords.splice(0, 1);
+
+  for (const coord of lineCoords) {
+    const coordX = coord[0];
+    const coordY = coord[1];
+    distance = distance + Math.round(computeDistance(lastX, lastY, coordX, coordY) / 100) / 10;
+    if (coordX < boundingBoxMinX) {
+      boundingBoxMinX = coordX;
     }
+    if (coordX > boundingBoxMaxX) {
+      boundingBoxMaxX = coordX;
+    }
+    if (coordY < boundingBoxMinY) {
+      boundingBoxMinY = coordY;
+    }
+    if (coordY > boundingBoxMaxY) {
+      boundingBoxMaxY = coordY;
+    }
+    lastX = coordX;
+    lastY = coordY;
+  }
 
-    const center = {
-      type: 'Feature',
-      geometry: {type: 'Point', coordinates: [(boundingBoxMinX + boundingBoxMaxX) / 2, (boundingBoxMinY + boundingBoxMaxY) / 2]},
-      properties: {distance}
-    };
-    functions.logger.log(`new center for ${context.params.documentId}: ${JSON.stringify(center)}`);
-    return admin.firestore().collection('centers').doc(context.params.documentId).set(center);
-  });
+  const center = {
+    type: 'Feature',
+    geometry: {type: 'Point', coordinates: [(boundingBoxMinX + boundingBoxMaxX) / 2, (boundingBoxMinY + boundingBoxMaxY) / 2]},
+    properties: {distance}
+  };
+  functions.logger.log(`new center for ${context.params.documentId}: ${JSON.stringify(center)}`);
+  return admin.firestore().collection('centers').doc(context.params.documentId).set(center);
+}
 
 function toRadians(value: number) {
   return value * Math.PI / 180;
